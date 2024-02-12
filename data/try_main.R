dbcee33179ab53f30aea59c6bcec84fafe1b9cc1
Guillermo Martínez <guillelozabala@gmail.com>
2024-02-12 17:35:29 +0100

    ee

---
+++ data/try_main.R
@@ -0,0 +1,8 @@
+
+install.packages("reticulate")
+
+library(reticulate)
+use_python("C:/Users/guill/AppData/Local/Programs/Python/Python311/python.exe")
+py_run_file("C:/Users/guill/Documents/GitHub/masters_thesis/data/minwage.py")
+
+py_run_file("C:/Users/guill/Documents/GitHub/masters_thesis/data/ahh.py")
