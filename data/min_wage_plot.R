824666b6f807bded24837b02eb9c47d8beb24c45
Guillermo Martínez <guillelozabala@gmail.com>
2024-02-28 15:24:27 +0100

    plots
    cleaner  code

--- data/min_wage_plot.R
+++
@@ -1,7 +0,0 @@
-
-rm(list=ls()) 
-
-library(tidyverse)
-
-setwd(dirname(rstudioapi::getSourceEditorContext()$path))
-minwg <- read.csv(paste(getwd(),"minwage_clean_ub.csv",sep = "/"),header=TRUE,sep=",")
